44c01fe3755f806e6196b299273e380d7ac35f62
miki31 <it.misha2016@gmail.com>
2020-03-15 01:41:12 +0200

    initial React

--- src/main/java/com/family/wallet/WalletApplication.java
+++ src/main/java/com/family/wallet/WalletApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalletApplication {
+    //test
 
     public static void main(String[] args) {
         SpringApplication.run(WalletApplication.class, args);
