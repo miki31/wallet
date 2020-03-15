5cd17f7ef2b4f4f9a90a68a2954e6c449553de41
miki31 <it.misha2016@gmail.com>
2020-03-15 19:16:01 +0200

    install bootstrap - v.1 --> test

--- wallet-react/src/App.js
+++ wallet-react/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends Component {
     state = {
         isLoading: true,
@@ -28,18 +30,28 @@ class App extends Component {
 
         return (
             <div className="App">
-                {/*<header className="App-header">*/}
-                {/*    <img src={logo} className="App-logo" alt="logo" />*/}
-                {/*    <div>*/}
-                <h1>Привіт марічка)))</h1>
-                <h2>Hello WORLD _____ in REACT and java_SPRING!!!!!!!!</h2>
-                {groups.map(group =>
-                    <div key={group.age}>
-                        {group.name}
-                    </div>
-                )}
-                {/*    </div>*/}
-                {/*</header>*/}
+                <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <div>
+                            <h1>Привіт марічка)))</h1>
+                            <h2>Hello WORLD _____ in REACT and java_SPRING!!!!!!!!</h2>
+                            {groups.map(group =>
+                                <div key={group.age}>
+                                    {group.name}
+                                </div>
+                            )}
+
+                            <div className="alert alert-success" role="alert">
+                                <h4 className="alert-heading">Well done!</h4>
+                                <p>Aww yeah, you successfully read this important alert message. This example text is
+                                    going to run a bit longer so that you can see how spacing within an alert works with
+                                    this kind of content.</p>
+                                <hr/>
+                                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep
+                                    things nice and tidy.</p>
+                            </div>
+                        </div>
+                </header>
             </div>
         );
     };
