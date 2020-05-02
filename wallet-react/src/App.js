793a6469e4de3bf28657b7efc5e93173bab13069
miki31 <it.misha2016@gmail.com>
2020-05-02 23:04:31 +0300

    add Alert from Reactstrap

--- wallet-react/src/App.js
+++ wallet-react/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Alert from "reactstrap/es/Alert";
+
 class App extends Component {
     state = {
         isLoading: true,
@@ -51,6 +53,18 @@ class App extends Component {
                                     things nice and tidy.</p>
                             </div>
                         </div>
+                    <Alert color="danger">
+                        <h4 className="alert-heading">Well done!</h4>
+                        <p>
+                            Aww yeah, you successfully read this important alert message. This example text is going
+                            to run a bit longer so that you can see how spacing within an alert works with this kind
+                            of content.
+                        </p>
+                        <hr />
+                        <p className="mb-0">
+                            Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+                        </p>
+                    </Alert>
                 </header>
             </div>
         );
