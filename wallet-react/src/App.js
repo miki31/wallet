30ebee633140641619591199f8d349558bbb25c1
miki31 <it.misha2016@gmail.com>
2020-05-02 22:51:12 +0300

    revert

--- wallet-react/src/App.js
+++ wallet-react/src/App.js
@@ -1,76 +1,80 @@
-import React, { useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Alert } from 'reactstrap';
 
-const url = '/api/models';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const App = () => {
-    const [ isLoading, setLoading ] = useState(true);
-    const [ groups, setGroups] = useState([]);
+class App extends Component {
+    state = {
+        isLoading: true,
+        groups: []
+    };
 
-    async function fetchUrl() {
-        const response = await fetch(url);
-        const json = await response.json();
-        setGroups(json);
+    //test revert
 
-        setTimeout(() => {
-            setLoading(false);
-        }, 1500);
-    }
+    async componentDidMount() {
+        const response = await fetch('/api/models');
+        const body = await response.json();
+        console.log(JSON.stringify(body));
+        // console.log(body);
+        // console.log("body");
+        this.setState({groups: body, isLoading: false});
+    };
 
-    useEffect(() => {
-        fetchUrl();
-    }, []);
+    render() {
+        const {groups, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <div>
+                            <h1>Привіт марічка)))</h1>
+                            <h2>Hello WORLD _____ in REACT and java_SPRING!!!!!!!!</h2>
+                            {groups.map(group =>
+                                <div key={group.age}>
+                                    {group.name}
+                                </div>
+                            )}
+
+                            <div className="alert alert-success" role="alert">
+                                <h4 className="alert-heading">Well done!</h4>
+                                <p>Aww yeah, you successfully read this important alert message. This example text is
+                                    going to run a bit longer so that you can see how spacing within an alert works with
+                                    this kind of content.</p>
+                                <hr/>
+                                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep
+                                    things nice and tidy.</p>
+                            </div>
+                        </div>
+                </header>
+            </div>
+        );
+    };
 
 
-    // async componentDidMount() {
-    //     const response = await fetch('/api/models');
-    //     const body = await response.json();
-    //     console.log(JSON.stringify(body));
-    //     // console.log(body);
-    //     // console.log("body");
-    //     this.setState({groups: body, isLoading: false});
-    // };
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
+}
 
-    return (
-        <div className="App">
-            { isLoading ? (
-                <p>Loading...</p>
-            ) :
-                (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <div>
-                    <h1>Привіт марічка)))</h1>
-                    <h2>Hello WORLD _____ in REACT and java_SPRING!!!!!!!!</h2>
-
-
-                    <div className="alert alert-success" role="alert">
-                        <h4 className="alert-heading">Well done!</h4>
-                        <p>Aww yeah, you successfully read this important alert message. This example text is
-                            going to run a bit longer so that you can see how spacing within an alert works with
-                            this kind of content.</p>
-                        <hr/>
-                        <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep
-                            things nice and tidy.</p>
-                    </div>
-                </div>
-
-                <Alert color="warning">
-                    <h4 className="alert-heading">Hey, nice to see you</h4>
-                    <p>
-                        Aww yeah, you successfully read this important alert message. This example
-                        text is going to run a bit longer so that you can see how spacing within an
-                        alert works with this kind of content.
-                    </p>
-                    <hr />
-                    <p className="mb-0">
-                        Whenever you need to, be sure to use margin utilities to keep things nice
-                        and tidy.
-                    </p>
-                </Alert>
-            </header>)}
-        </div>
-    );
-};
+export default App;
